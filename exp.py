1eef2aaf8676f2d54d52a3ad750b5e6cf10391fc
vidulkumar <30744970+vidulkumar@users.noreply.github.com>
2017-09-05 12:04:48 +0530

    first file uploaded

---
+++ exp.py
@@ -0,0 +1,9 @@
+def main():
+    print('enter a list')
+    print('press 3 spaces for termination')
+    while(key!=''):
+        key=int(input())
+        lst.append=key
+    print(lst)   
+if __name__=='__main__':
+    main()
