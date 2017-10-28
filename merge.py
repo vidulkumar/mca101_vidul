6252aeb5e266898f38a41338cbf4cddbb2fb4a71
vidulkumar <30744970+vidulkumar@users.noreply.github.com>
2017-10-28 18:36:29 +0530

    merge
    this program merges two sorted list

--- merge.py
+++ merge.py
@@ -5,27 +5,23 @@ input parameters: lis1 and list2 two sorted lists
 approach: Recursion
 
 '''
-
-
-
 import pdb
 
 def merge(list1,list2,list3=[],idx1=0,idx2=0):
     #pdb.set_trace()
     if(len(list3)==len(list1)+len(list2)):
         return list3
-    if len(list1)==idx1:
+    elif len(list1)==idx1:
         list3.extend(list2[idx2:])
-        return list3
-        
-    if len(list2)==idx2 :
+        return list3  
+    elif len(list2)==idx2 :
         list3.extend(list1[idx1:])
         return list3    
     elif list1[idx1]>=list2[idx2]:
         list3.append(list2[idx2])
         idx2=idx2+1
         return merge(list1,list2,list3,idx1,idx2)
-    elif list1[idx1]<=list2[idx2]:
+    else:
         list3.append(list1[idx1])
         idx1=idx1+1
         return merge(list1,list2,list3,idx1,idx2)
