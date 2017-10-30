b9bf00f7adbe393d12c3ed8e0e6afb94b205a26c
Chirag Bohet <bohet.chirag@gmail.com>
2017-10-30 23:53:23 +0530

    Added documentation and case if disks  = 0
    Added proper documentation and the case if no. of disks entered is 0

--- hanoi.py
+++ hanoi.py
@@ -1,5 +1,15 @@
 def hanoi(n,source,spare,target):
-    if n==1:
+    '''
+    Objective : To generate movements needed to solve Tower of Hanoi
+    Input Variables : 
+        n : No. of disks
+        source : The source pole ie. The starting pole where all the disks are originally
+        spare : The spare pole which will be used to store disks temporarily
+        target : The target pole ie. The destination pole where the disks are meant to go.
+    '''
+    if n==0:
+      return  
+    elif n==1:
         print('move disk from ',source,' to',target)
     else:
         hanoi(n-1,source,target,spare)
