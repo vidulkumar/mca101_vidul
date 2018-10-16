8e87ea4b776abefa9d9b57a9f815b7030a526565
Mohit Arora <32641304+mohitarora3@users.noreply.github.com>
2018-10-16 15:29:59 +0530

    Update area of triangle.py

--- area of triangle.py
+++ area of triangle.py
@@ -3,10 +3,10 @@ def areaTriangle(base,height):
       '''
       objective: To Compute area of a Triangle 
       input parameters 
-      length : base if the Triangle
-      breadth: height of the Triangle
-      approach: multiply base and height and 1/2
-      return value : Area of the Triangle
+      length :base if the Triangle
+      breadth:height of the Triangle
+      approach:multiply base and height and 1/2
+      return value :Area of the Triangle
       '''
       area = (1/2) * base * height
       return area
@@ -19,8 +19,8 @@ def main():
     height: breadth of the Triangle
     approach:
     '''
-    base = int(input("enter base of Triangle "))
-    height= int(input("enter height of Triangle "))
+    base =int(input("enter base of Triangle "))
+    height=int(input("enter height of Triangle "))
     print ("base of triangle : ", base)
     print ("height of Triangle : ", height)
     print ('id(base) ', id(base) )
